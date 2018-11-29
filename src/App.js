81e6d952008861c02caec7c697053ad05313d5a8
Michelle Yenny <mmy491@gmail.com>
2018-11-28 21:11:06 -0500

    added state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: 'Learn HTML & CSS',
+          complete: true
+        },
+        {
+          id: 2,
+          text: 'Master React',
+          complete: false
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>One List</h1>
         </header>
+        <main>
+          <ul className="one-list">
+            {this.state.todos.map((todo, index) => {
+              return <></>
+            })}
+            <li className="completed">Review at 2</li>
+            <li>Watch Videos</li>
+            <li>Eat Dinner</li>
+            <li>Code</li>
+            <li>Sleep</li>
+          </ul>
+          <input type="text" placeholder="Whats up?" />
+        </main>
+        <footer>
+          <p>
+            <img src={logo} height="42" alt="logo" />
+          </p>
+          <p>&copy; 2018 Suncoast Developers Guild</p>
+        </footer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
